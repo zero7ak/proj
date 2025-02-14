d68414796773d2469953228c344867e3a0a2fd9d
기다림 <gidalimforsomeday@gmail.com>
2025-02-15 04:04:26 +0900

    chore: 필요없는 파일 추가/제거 및 글로벌css 기본설정

--- livechat/tailwind.config.ts
+++ livechat/tailwind.config.ts
@@ -1,18 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
+  darkMode: 'class',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)'
       },
-    },
+      fontFamily: {
+        globalFont: 'var(--globalFont)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 } satisfies Config;
