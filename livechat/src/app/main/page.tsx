3e464540db2f1c673d4787e535639e63efe8d92a
기다림 <gidalimforsomeday@gmail.com>
2025-02-15 04:59:25 +0900

    feat: DefaultPage 추가 #8

---
+++ livechat/src/app/main/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DefaultPage = () => {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      DefaultPage
+    </div>
+  );
+};
+
+export default DefaultPage;
